30e44d285176bb299560264bf58f6b20d0fd9bed
sss <sss@gmail.com>
2023-10-01 06:30:46 +0700

    xx

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const runApp = async () => {
     apiKey: "OsZJWLU2BCq9DCtNNJb2QL0pcvBCYavmM3f2HmdqHMbHs27VZVanDaNJHML6ndXr"
   });
 
-  const address = "0x85C75455e529C1d5592e828b4c106191EFdE5B8C";
-
+  const address = "0xb8e9453975a2af11c23e6b0c11219e0b1f36b9642e67660660250e46f0361b7a";
+  
   const chain = EvmChain.MUMBAI;
   const limit = 10;
   const response = await Moralis.AptosApi.collections.getNFTCollectionsByCreator({
